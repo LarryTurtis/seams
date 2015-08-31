085fd157e2437bff2706f3b305831a68191551c5
Gary Kertis <gary.kertis@gmail.com>
2015-08-31 17:03:31 -0400

    Move file logic and add protective filters.

---
+++ fileUpload.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var multer = require("multer");
+
+var storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './uploads')
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.originalname)
+    }
+});
+
+var limits = {
+    fields: 100,
+    fileSize: 5000000,
+    files: 10,
+    parts: 110
+}
+
+var fileFilter = function(req, file, cb) {
+    if (file.mimetype !== "image/jpeg" && file.mimetype !== "image/png" && file.mimetype != "image/gif") {
+        cb(null, false)
+    } else {
+    	cb(null, true);
+    }
+};
+
+var upload = multer({
+    storage: storage,
+    limits: limits,
+    fileFilter: fileFilter
+});
+
+module.exports = upload;
--- server.js
+++ server.js
@@ -1,37 +1,21 @@
-var http = require("http");
-var express = require("express");
+var http = require("http"),
+  express = require("express"),
+  MongoClient = require("mongodb").MongoClient,
+  assert = require("assert"),
+  ObjectId = require("mongodb").ObjectID,
+  upload = require('./fileUpload.js'),
+  url = "mongodb://localhost:27017/test",
+  bodyParser = require("body-parser"),
+  app = express();
 
-var MongoClient = require("mongodb").MongoClient;
-var assert = require("assert");
-var ObjectId = require("mongodb").ObjectID;
-var url = "mongodb://localhost:27017/test";
-
-var multer = require("multer");
-
-var storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, './uploads')
-    },
-    filename: function(req, file, cb) {
-        cb(null, file.originalname)
-    }
-})
-
-var upload = multer({
-    storage: storage
-})
-
-var app = express();
-
-bodyParser = require("body-parser")
 app.use(bodyParser.json())
-    .use(bodyParser.urlencoded())
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(express.static(__dirname));
 
 
 var insertDocument = function(db, callback, data) {
     db.collection('fruits').insertOne(data, function(err, result) {
         assert.equal(err, null);
-        console.log("Inserted a document into the fruits collection.");
         callback(result);
     });
 };
@@ -50,10 +34,8 @@ var findDocument = function(db, callback, filter) {
 };
 
 var createRecord = function(req, res) {
-    console.log("received!", req.url, req.headers, req.body);
     MongoClient.connect(url, function(err, db) {
         assert.equal(null, err);
-        console.log("Connected correctly to server");
         insertDocument(db, function() {
             db.close();
         }, req.body);
@@ -62,23 +44,18 @@ var createRecord = function(req, res) {
 };
 
 var getRecord = function(req, res) {
-    console.log("received!", req.url, req.headers, req.body);
     MongoClient.connect(url, function(err, db) {
         assert.equal(null, err);
-        console.log("Connected correctly to server");
         findDocument(db, function(result) {
             db.close();
-            console.log("done", result);
             res.send(result);
         }, req.body);
     });
 };
 
-app.use(express.static(__dirname));
-
 app.post("/api/dbCreate", createRecord);
 app.post("/api/db", getRecord);
-app.post("/api/upload", upload.single('avatar'), function(req, res, next) {
+app.post("/api/upload", upload.single('avatar'), function(req, res) {
     res.send(req.file);
 });
 
