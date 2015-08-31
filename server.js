079763a3f7f5025b2c3c78ff6233acb46e645531
Gary Kertis <gary.kertis@gmail.com>
2015-08-31 16:18:36 -0400

    Update server js to allow file uploads.

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 var http = require("http");
 var express = require("express");
-var app = express();
-app.use(express.bodyParser()); // support json encoded bodies
 
 var MongoClient = require("mongodb").MongoClient;
 var assert = require("assert");
 var ObjectId = require("mongodb").ObjectID;
 var url = "mongodb://localhost:27017/test";
+
+var multer = require("multer");
+
+var storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './uploads')
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
+
+var upload = multer({
+    storage: storage
+})
+
+var app = express();
+
+bodyParser = require("body-parser")
+app.use(bodyParser.json())
+    .use(bodyParser.urlencoded())
+
 
 var insertDocument = function(db, callback, data) {
     db.collection('fruits').insertOne(data, function(err, result) {
@@ -18,15 +38,15 @@ var insertDocument = function(db, callback, data) {
 
 var findDocument = function(db, callback, filter) {
     var result = [];
-   var cursor =db.collection('fruits').find(filter);
-   cursor.each(function(err, doc) {
-      assert.equal(err, null);
-      if (doc != null) {
-         result.push(doc);
-      } else {
-         callback(result);
-      }
-   });
+    var cursor = db.collection('fruits').find(filter);
+    cursor.each(function(err, doc) {
+        assert.equal(err, null);
+        if (doc != null) {
+            result.push(doc);
+        } else {
+            callback(result);
+        }
+    });
 };
 
 var createRecord = function(req, res) {
@@ -54,14 +74,16 @@ var getRecord = function(req, res) {
     });
 };
 
-app.post("/api/dbCreate", createRecord);
-app.post("/api/db", getRecord);
 app.use(express.static(__dirname));
 
+app.post("/api/dbCreate", createRecord);
+app.post("/api/db", getRecord);
+app.post("/api/upload", upload.single('avatar'), function(req, res, next) {
+    res.send(req.file);
+});
 
 var server = http.createServer(app).listen(2323, function() {
     var host = server.address().address;
     var port = server.address().port;
     console.log("Node.js Listening on port: %s:%s", host, port);
 });
-
