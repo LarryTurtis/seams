0f3f8317b209149f5c3d250a44111e7fc43151e9
Gary Kertis <gary.kertis@gmail.com>
2015-09-08 18:06:38 -0400

    Working to auth for DB record create.

--- server.js
+++ server.js
@@ -18,11 +18,24 @@ app.set('view engine', 'jade');
 app
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: true }))
-  .use(express.static(__dirname))
   .use(expressSession({secret: 'mySecretKey'}))
   .use(passport.initialize())
   .use(passport.session())
   .use(flash());
+
+var isAuthenticated = function (req, res, next) {
+  // if user is authenticated in the session, call the next() to call the next request handler 
+  // Passport adds this method to request object. A middleware is allowed to add properties to
+  // request and response objects
+  if (req.isAuthenticated()) {
+    console.log("is authed");
+    res.setHeader("Authed", "authed");
+    return next();
+  }
+  // if the user is not authenticated then redirect him to the login page
+  res.redirect('/');
+}
+
 
 var initPassport = require('./lib/passport/init');
 initPassport(passport);
@@ -30,13 +43,20 @@ initPassport(passport);
 var routes = require('./lib/routes/index')(passport);
 app.use('/', routes);
 
-app.post("/api/dbCreate", database.createRecord);
+app.post("/api/dbCreate", isAuthenticated, database.createRecord);
 app.post("/api/db", database.getRecord);
 
 app.post("/api/upload", upload.single("avatar"), function(req, res) {
     res.send(req.file);
 });
 
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+if (app.get('env') === 'development') {
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
@@ -44,6 +64,7 @@ app.use(function(err, req, res, next) {
         error: err
     });
 });
+}
 
 var server = http.createServer(app).listen(2323, function() {
     var host = server.address().address;
