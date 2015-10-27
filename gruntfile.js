47e2859e90b0b601f3494e6bdd2af25e25f9b051
Gary Kertis <gary.kertis@gmail.com>
2015-10-27 17:32:22 -0400

    Fix bug.

--- gruntfile.js
+++ gruntfile.js
@@ -29,6 +29,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
 
     // Default task(s).
-    grunt.registerTask('default', ['processhtml', 'copy']);
+    grunt.registerTask('default', ['copy', 'processhtml']);
 
 };
